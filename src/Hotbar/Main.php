521f21ad073e5efc3fbbad69ada1edf0be9eb1ee
Eric <uhericc@gmail.com>
2021-07-15 05:48:28 -0700

    Update Main.php
    Unnecessary

--- src/Hotbar/Main.php
+++ src/Hotbar/Main.php
@@ -4,10 +4,8 @@ declare(strict_types=1);
 
 namespace Hotbar;
 
-use pocketmine\Player;
 use pocketmine\event\Listener;
 use pocketmine\plugin\PluginBase;
-use pocketmine\event\player\PlayerInteractEvent;
 
 class Main extends PluginBase implements Listener {
 
